5187ed08e7378133155bb269628626f2c0fed873
Puja Nikam <pujasantoshnikam@gmail.com>
2025-12-25 19:14:18 +0530

    feat: displayed mock x, y, z values and acceleration magnitude

--- src/main.py
+++ src/main.py
@@ -1,1 +1,10 @@
-print("Project setup successful")
+from sensors.mpu6050_simulator import get_motion_data
+from detection.motion_analysis import calculate_magnitude
+import time
+
+while True:
+    ax, ay, az = get_motion_data()
+    magnitude = calculate_magnitude(ax, ay, az)
+
+    print(f"AX:{ax:.2f} AY:{ay:.2f} AZ:{az:.2f} | MAG:{magnitude:.2f}")
+    time.sleep(1)
